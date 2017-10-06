883aace18eb96eee908aa3fb18a6636f2ad91ae1
anfriis <af@andersfr.dk>
2017-10-06 17:54:12 +0200

    Add files

---
+++ XMSegmentedControl/XMSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  XMSegmentedControl.h
+//  XMSegmentedControl
+//
+//  Created by Anders Friis on 06/10/2017.
+//  Copyright © 2017 Cromian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMSegmentedControl.
+FOUNDATION_EXPORT double XMSegmentedControlVersionNumber;
+
+//! Project version string for XMSegmentedControl.
+FOUNDATION_EXPORT const unsigned char XMSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMSegmentedControl/PublicHeader.h>
+
+
---
+++ XMSegmentedControlDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMSegmentedControl
+//
+//  Created by Anders Friis on 06/10/2017.
+//  Copyright © 2017 Cromian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
